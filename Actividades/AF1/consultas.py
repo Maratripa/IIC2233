d5040c26fff1f5f7031f939e23f2109246c3d8e3
Martin Atria Paublo <titin@pop-os.localdomain>
2022-03-17 15:56:18 -0300

    avance consultas.py

--- Actividades/AF1/consultas.py
+++ Actividades/AF1/consultas.py
@@ -1,14 +1,31 @@
+from collections import namedtuple
+
+Plato = namedtuple("Plato_type", ["nombre", "categoria", "tiempo", "precio", "ingredientes"])
+
 # --- EJEMPLO --- #
 # [Plato1, Plato2, Plato2, Plato4]
 # pasa a ser
 # {"Categoria1": [Plato3, Plato2], "Categoria2": [Plato1, Plato4]}
 def platos_por_categoria(lista_platos: list) -> dict:
-    pass
+    return_dict = {}
+    for plato in lista_platos:
+        if plato.categoria in return_dict:
+            return_dict[plato.categoria].append(plato)
+        else:
+            return_dict[plato.categoria] = [plato]
+    
+    return return_dict
 
 
 # Debe devolver los platos que no tengan ninguno de los ingredientes descartados
 def descartar_platos(ingredientes_descartados: set, lista_platos: list):
-    pass
+    returned_platos = []
+
+    for plato in lista_platos:
+        if len(plato.ingredientes & ingredientes_descartados) == 0:
+            returned_platos.append(plato)
+    
+    return returned_platos
 
 
 # --- EXPLICACION --- #
@@ -32,4 +49,18 @@ def preparar_plato(plato, ingredientes: dict) -> bool:
 #  de la lista de platos.
 # precio total, tiempo total, cantidad de platos, platos
 def resumen_orden(lista_platos: list) -> dict:
-    pass
+    return_dict = {
+        "precio total" : 0,
+        "tiempo total" : 0,
+        "cantidad de platos" : 0,
+        "platos" : []
+    }
+
+    for p in lista_platos:
+        return_dict["precio total"] += p.precio
+        return_dict["tiempo total"] += p.tiempo
+        return_dict["cantidad de platos"] += 1
+        return_dict["platos"].append(p.nombre)
+    
+    return return_dict
+
