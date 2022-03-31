2b07e506c0a47ce9ab46fc0e6448dac6ab2d7727
Martin Atria Paublo <maratripa@gmail.com>
2022-03-31 15:21:56 -0300

    completar clases construcciones.py

--- Actividades/AS1/construcciones.py
+++ Actividades/AS1/construcciones.py
@@ -2,12 +2,12 @@ from abc import ABC, abstractmethod
 from random import choice, randint, random
 from unidades import Guerrero, Mago, MagoGuerrero
 from parametros import PROB_CRITICO_MURO, PROB_CRITICO_CATAPULTA, \
-                       HP_MUROCATAPULTA, PROB_CRITICO_MURO_CATAPULTA, \
-                       HP_BARRACAS, HP_MURO, HP_CATAPULTA
+    HP_MUROCATAPULTA, PROB_CRITICO_MURO_CATAPULTA, \
+    HP_BARRACAS, HP_MURO, HP_CATAPULTA
 
 
 # Recuerda que es una clase abstracta
-class Estructura:
+class Estructura(ABC):
 
     def __init__(self, edad, *args):
         # No modificar
@@ -17,20 +17,40 @@ class Estructura:
 
     # ---------------
     # Completar los métodos abstractos aquí
+    @abstractmethod
+    def asignar_atributos_segun_edad(self):
+        pass
 
+    @abstractmethod
+    def accion(self):
+        pass
+
+    @abstractmethod
+    def avanzar_edad(self):
+        pass
     # ---------------
 
 
 # Recuerda completar la herencia
-class Barracas:
+class Barracas(Estructura):
 
     def __init__(self, *args):
         # Completar
-        pass
+        super().__init__(*args)
+        self.hp = HP_BARRACAS
 
     # ---------------
     # Completar los métodos aquí
+    def asignar_atributos_segun_edad(self):
+        if self.edad == "Media":
+            self.unidades = ["Guerrero", "Mago"]
+        elif self.edad == "Moderna":
+            self.unidades = ["Guerrero", "Mago", "MagoGuerrero"]
 
+    def avanzar_edad(self):
+        if self.edad == "Media":
+            self.edad = "Moderna"
+            self.unidades.append("MagoGuerrero")
     # ---------------
 
     def accion(self):
@@ -50,28 +70,62 @@ class Barracas:
 
 
 # Recuerda completar la herencia
-class Muro:
+class Muro(Estructura):
 
     def __init__(self, *args):
         # Completar
-        pass
+        super().__init__(*args)
+        self.hp = HP_MURO
 
     # ---------------
     # Completar los métodos aquí
+    def asignar_atributos_segun_edad(self):
+        if self.edad == "Media":
+            self.reparacion = [20, 80]
+        elif self.edad == "Moderna":
+            self.reparacion = [40, 100]
 
+    def accion(self):
+        reconstruccion = randint(*self.reparacion)
+        if random() < PROB_CRITICO_MURO:
+            return reconstruccion * 2
+        else:
+            return reconstruccion
+
+    def avanzar_edad(self):
+        if self.edad == "Media":
+            self.edad = "Moderna"
+            self.reparacion = [40, 100]
     # ---------------
 
 
 # Recuerda completar la herencia
-class Catapulta:
+class Catapulta(Estructura):
 
     def __init__(self, *args):
         # Completar
-        pass
+        super().__init__(*args)
+        self.hp = HP_CATAPULTA
 
     # ---------------
     # Completar los métodos aquí
+    def asignar_atributos_segun_edad(self):
+        if self.edad == "Media":
+            self.ataque = [40, 100]
+        elif self.edad == "Moderna":
+            self.ataque = [80, 140]
 
+    def accion(self):
+        ataque = randint(*self.ataque)
+        if random() < PROB_CRITICO_CATAPULTA:
+            return ataque * 2
+        else:
+            return ataque
+
+    def avanzar_edad(self):
+        if self.edad == "Media":
+            self.edad = "Moderna"
+            self.ataque = [80, 140]
     # ---------------
 
 
@@ -80,11 +134,29 @@ class MuroCatapulta(Muro, Catapulta):
 
     def __init__(self, *args):
         # Completar
-        pass
+        super().__init__(*args)
+        self.hp = HP_MUROCATAPULTA
 
     # ---------------
     # Completar los métodos aquí
+    def asignar_atributos_segun_edad(self):
+        Muro.asignar_atributos_segun_edad(self)
+        Catapulta.asignar_atributos_segun_edad(self)
 
+    def accion(self):
+        reparacion = randint(*self.reparacion)
+        ataque = randint(*self.ataque)
+
+        if random() < PROB_CRITICO_MURO_CATAPULTA:
+            return (round(reparacion * 5 / 2), round(ataque * 5 / 2))
+        else:
+            return (reparacion, ataque)
+
+    def avanzar_edad(self):
+        if self.edad == "Media":
+            self.edad = "Moderna"
+            self.reparacion = [40, 100]
+            self.ataque = [80, 140]
     # ---------------
 
 
@@ -92,4 +164,13 @@ if __name__ == "__main__":
     # ---------------
     # En esta sección puedes probar tu codigo
     # ---------------
-    pass
+    mc = MuroCatapulta("Moderna")
+
+    print(mc.accion())
+    print(mc.edad)
+    print(mc.reparacion)
+    print(mc.ataque)
+    mc.avanzar_edad()
+    print(mc.edad)
+    print(mc.reparacion)
+    print(mc.ataque)
