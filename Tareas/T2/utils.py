e53584d994f506f73a7a86968d6229dfc8a8881d
Martin Atria Paublo <maratripa@gmail.com>
2022-05-11 21:38:22 -0400

    soporte para encapsular layouts

--- Tareas/T2/utils.py
+++ Tareas/T2/utils.py
@@ -1,11 +1,14 @@
 from PyQt5.QtCore import QObject
-from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout, QLayout, QWidget
 
 
-def encapsular_h(widget: QObject) -> QHBoxLayout:
+def encapsular_h(object: QObject) -> QHBoxLayout:
     hbox = QHBoxLayout()
     hbox.addStretch(1)
-    hbox.addWidget(widget)
+    if isinstance(object, QWidget):
+        hbox.addWidget(object)
+    elif isinstance(object, QLayout):
+        hbox.addLayout(object)
     hbox.addStretch(1)
 
     return hbox
