11118b1c57aa41f0a6a330f623730ac9645bf0e8
Martin Atria Paublo <maratripa@gmail.com>
2022-06-02 15:22:32 -0400

    terminado

--- Actividades/AS4/Parte1/fila_pizzas.py
+++ Actividades/AS4/Parte1/fila_pizzas.py
@@ -1,34 +1,69 @@
 class Cliente:
     def __init__(self, nombre):
-        # Completar
-        pass
-
+        self.nombre = nombre
+        self.siguiente = None
 
     def __str__(self):
         # NO MODIFICAR
         return self.nombre
 
+
 class FilaPizza:
     def __init__(self):
-        # Completar
-        pass
-
+        self.primero = None
+        self.ultimo = None
+        self.largo = 0
 
     def llega_cliente(self, cliente: Cliente):
-        # Completar
-        pass
+        if self.largo == 0:
+            self.primero = cliente
+            self.ultimo = cliente
+            self.largo += 1
+        else:
+            self.ultimo.siguiente = cliente
+            self.ultimo = cliente
+            self.largo += 1
 
     def alguien_se_cuela(self, cliente: Cliente, posicion: int):
-        # Completar
-        pass
-    
-    def cliente_atendido(self):
-        # Completar
-        pass
+        if self.largo == 0:
+            self.primero = cliente
+            self.ultimo = cliente
+            self.largo += 1
+        elif posicion == 0:
+            cliente.siguiente = self.primero
+            self.primero = cliente
+            self.largo += 1
+        elif posicion >= self.largo:
+            self.llega_cliente(cliente)
+        else:
+            tmp = self.primero
+            for i in range(posicion - 1):
+                tmp = tmp.siguiente
+            cliente.siguiente = tmp.siguiente
+            tmp.siguiente = cliente
+            self.largo += 1
+
+    def cliente_atendido(self) -> Cliente:
+        if self.largo == 1:
+            cliente = self.primero
+            self.primero = None
+            self.ultimo = None
+            self.largo -= 1
+            return cliente
+        elif self.largo > 1:
+            cliente = self.primero
+            self.primero = self.primero.siguiente
+            self.largo -= 1
+            return cliente
 
     def __str__(self):
-        # Completar
-        pass
+        string = self.primero.__str__()
+        tmp = self.primero.siguiente
+        for _ in range(self.largo - 1):
+            string += " -> " + tmp.__str__()
+            tmp = tmp.siguiente
+
+        return string
 
 
 if __name__ == "__main__":
--- Actividades/AS4/Parte2/recorrido_campus.py
+++ Actividades/AS4/Parte2/recorrido_campus.py
@@ -1,6 +1,7 @@
 from Parte2.campus import Lugar
 from collections import deque
 
+
 def comprobar_chismoso(lugar: Lugar):
     # NO MODIFICAR
     for ayudante in lugar.ayudantes:
@@ -10,47 +11,163 @@ def comprobar_chismoso(lugar: Lugar):
 
 
 def bfs_iterativo(inicio: Lugar, final: Lugar):
-    # Completar
-    pass
+    visitados = []
+    cola = deque()
+    cola.append(inicio)
+
+    while len(cola) > 0:
+        nodo = cola.popleft()
+
+        if nodo == final:
+            return True
+
+        if nodo in visitados:
+            continue
+
+        visitados.append(nodo)
+
+        if not comprobar_chismoso(nodo):
+            for vecino in nodo.vecinos:
+                if vecino not in visitados:
+                    cola.append(vecino)
+
+    return False
 
 
 def dfs_iterativo(inicio: Lugar, final: Lugar):
-    # Completar
-    pass
+    visitados = set()
+    stack = []
+    stack.append(inicio)
+
+    while len(stack) > 0:
+        nodo = stack.pop()
+
+        if nodo == final:
+            return True
+
+        if nodo in visitados:
+            continue
+
+        visitados.add(nodo)
+
+        if not comprobar_chismoso(nodo):
+            for vecino in nodo.vecinos:
+                if vecino not in visitados:
+                    stack.append(vecino)
+
+    return False
+
 
 def bfs_iterativo_largo(inicio: Lugar, final: Lugar):
-    # Completar 
-    pass
+    visitados = []
+    cola = deque()
+    cola.append(inicio)
+
+    inicio.nivel = 1
+
+    while len(cola) > 0:
+        nodo = cola.popleft()
+
+        if nodo == final:
+            return nodo.nivel
+
+        if nodo in visitados:
+            continue
+
+        visitados.append(nodo)
+
+        if not comprobar_chismoso(nodo):
+            for vecino in nodo.vecinos:
+                if vecino not in visitados:
+                    vecino.nivel = nodo.nivel + 1
+                    cola.append(vecino)
+
+    return -1
 
 
 def dfs_iterativo_largo(inicio: Lugar, final: Lugar):
-    # Completar 
-    pass
+    visitados = set()
+    stack = []
+    stack.append(inicio)
+
+    inicio.nivel = 1
+
+    while len(stack) > 0:
+        nodo = stack.pop()
+
+        if nodo == final:
+            return nodo.nivel
+
+        if nodo in visitados:
+            continue
+
+        visitados.add(nodo)
+
+        if not comprobar_chismoso(nodo):
+            for vecino in nodo.vecinos:
+                if vecino not in visitados:
+                    vecino.nivel = nodo.nivel + 1
+                    stack.append(vecino)
+
+    return -1
 
 
 def bfs_iterativo_camino(inicio: Lugar, final: Lugar):
-    # Utiliza este diccionario para implementar el camino. 
-    # Las llaves del diccionario es UN nodo vecino (NO un listado de todos los nodos vecinos) 
+    # Utiliza este diccionario para implementar el camino.
+    # Las llaves del diccionario es UN nodo vecino (NO un listado de todos los nodos vecinos)
     # y el valor el nodo en cuestion
     padres = dict()
     padres[inicio] = None
 
-    # Completar
-    pass
-    
+    visitados = []
+    cola = deque()
+    cola.append(inicio)
+
+    while len(cola) > 0:
+        nodo = cola.popleft()
+
+        if nodo in visitados:
+            continue
+
+        visitados.append(nodo)
+
+        if not comprobar_chismoso(nodo):
+            for vecino in nodo.vecinos:
+                if vecino not in visitados:
+                    cola.append(vecino)
+                    padres[vecino] = nodo
+
+    return creador_camino(padres, final)
+
 
 def dfs_iterativo_camino(inicio: Lugar, final: Lugar):
-    # Utiliza este diccionario para implementar el camino. 
-    # Las llaves del diccionario es UN nodo vecino (NO un listado de todos los nodos vecinos) 
+    # Utiliza este diccionario para implementar el camino.
+    # Las llaves del diccionario es UN nodo vecino (NO un listado de todos los nodos vecinos)
     # y el valor el nodo en cuestion
-    padres = dict()
     padres = dict()
     padres[inicio] = None
 
-    # Completar
-    pass
-    
-    
+    visitados = set()
+    stack = []
+    stack.append(inicio)
+
+    while len(stack) > 0:
+        nodo = stack.pop()
+
+        if nodo in visitados:
+            continue
+
+        visitados.add(nodo)
+
+        if not comprobar_chismoso(nodo):
+            for vecino in nodo.vecinos:
+                if vecino not in visitados:
+                    stack.append(vecino)
+                    padres[vecino] = nodo
+
+    return creador_camino(padres, final)
+
+
 def creador_camino(diccionario_padres, final):
     # NO MODIFICAR
     camino = []
@@ -74,6 +191,7 @@ def imprimir_camino(camino):
             recorrido = recorrido + f"[{lugar.nombre}]."
         contador += 1
     print(recorrido)
+
 
 if __name__ == "__main__":
     print("\nNO DEBES EJECUTAR AQUÍ EL PROGRAMA!!!!")
