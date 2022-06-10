45f08ad0ef38fda00b9e4c3cc1e8e1cd98c9c703
Martin Atria Paublo <maratripa@gmail.com>
2022-06-10 16:57:46 -0400

    Senales ventana juego

--- Tareas/T3/cliente/backend/interfaz.py
+++ Tareas/T3/cliente/backend/interfaz.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import pyqtSignal, QObject
 
 from frontend.ventana_inicio import VentanaInicio
 from frontend.ventana_espera import VentanaEspera
+from frontend.ventana_juego import VentanaJuego
 
 
 class Interfaz(QObject):
@@ -9,18 +10,23 @@ class Interfaz(QObject):
     senal_cargar_pantalla_espera = pyqtSignal(bool, list)
     #                                           (users)
     senal_actualizar_lista_usuarios = pyqtSignal(list)
+    #                                 (users)
+    senal_iniciar_partida = pyqtSignal(list)
 
     def __init__(self, parent):
         super().__init__()
 
         self.ventana_inicio = VentanaInicio()
         self.ventana_espera = VentanaEspera()
+        self.ventana_juego = VentanaJuego()
 
         #  CONEXIONES
         self.ventana_inicio.senal_enviar_usuario.connect(parent.enviar_mensaje)
+        self.ventana_espera.senal_iniciar_juego.connect(parent.enviar_mensaje)
 
         self.senal_cargar_pantalla_espera.connect(self.ventana_espera.cargar_pantalla)
         self.senal_actualizar_lista_usuarios.connect(self.ventana_espera.cargar_usuarios)
+        self.senal_iniciar_partida.connect(self.ventana_juego.init_gui)
 
     def mostrar_ventana_inicio(self):
         self.ventana_inicio.mostrar()
@@ -40,3 +46,9 @@ class Interfaz(QObject):
                 self.ventana_inicio.error_usuario(mensaje["error"])
         elif comando == "actualizar_lista_usuarios":
             self.senal_actualizar_lista_usuarios.emit(mensaje["usuarios"])
+        elif comando == "repuesta_iniciar_partida":
+            if mensaje["estado"] == "aceptado":
+                self.ventana_espera.esconder()
+                self.senal_iniciar_partida.emit(mensaje["usuarios"])
+            else:
+                print("ERROR FATAL")
